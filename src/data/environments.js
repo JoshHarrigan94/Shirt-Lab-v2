b7a60e91c8af61b4e878ded7edd51f252238be00
JoshHarrigan94 <145808535+JoshHarrigan94@users.noreply.github.com>
2026-06-16 18:02:14 +0100

    Delete src directory

--- src/data/environments.js
+++
@@ -1,6 +0,0 @@
-export const environments = {
-  temperate: { label: "Temperate Run", temperature: 18, humidity: 55, windSpeed: 8 },
-  hotDry: { label: "Hot Dry Trail", temperature: 31, humidity: 28, windSpeed: 12 },
-  hotHumid: { label: "Hot Humid City", temperature: 29, humidity: 78, windSpeed: 5 },
-  wetWindy: { label: "Cool Wet Wind", temperature: 11, humidity: 90, windSpeed: 18 }
-};
